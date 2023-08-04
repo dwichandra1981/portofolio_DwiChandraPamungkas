894002ce9aebde04fafeb7db3371e85c623f1bba
dwi chandra pamungkas <dwichandra1981@gmail.com>
2023-08-05 00:46:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      screens: {
+        'xs': '475px'
+      },
+
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+        dark : '#171717',
+      },
+    },
+  },
+  plugins: [],
+}
